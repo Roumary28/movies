0d507b3128fde0037bff54be12c4b0b6c920ee42
Danna Martinez <dannamartinez1995@gmail.com>
2024-10-31 18:26:16 -0300

    Create MovieController.java

---
+++ src/main/java/com/danna/movies/controllers/MovieController.java
@@ -0,0 +1,36 @@
+package com.danna.movies.controllers;
+
+import com.danna.movies.models.Movie;
+import com.danna.movies.repositories.MovieRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+    
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
